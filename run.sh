c279e49364e9ebd472354c94f8c51cc36df9f422
Joseph Sachs <jhalesachs@gmail.com>
2026-03-18 11:17:52 +0700

    [EID-2] Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,17 @@ WORKER_COUNT=${1:-1}
 shift  # Remove the first argument so getopts can process flags
 
 cd docker
+docker compose stop
 
 START_MINARE=false
 START_EVENNIA=false
+BUILD=false
 
-while getopts "me" flag; do
+while getopts "meb" flag; do
   case "${flag}" in
     m) START_MINARE=true ;;
     e) START_EVENNIA=true ;;
+    b) BUILD=true ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       exit 1
@@ -19,20 +22,51 @@ while getopts "me" flag; do
   esac
 done
 
-if $START_MINARE && $START_EVENNIA; then
-  echo "Starting Minare and Evennia..."
-  WORKER_COUNT=${WORKER_COUNT} docker compose up -d --scale worker=${WORKER_COUNT} haproxy app-coordinator infra worker mongodb redis kafka kafka-ui
-  docker compose up -d evennia
-elif $START_MINARE; then
+build_minare() {
+  echo "Building Minare..."
+  rm -rf logs/*.log
+  rm -rf data/{db,redis,kafka}/*
+  chmod 755 data/{db,redis,kafka}
+  mkdir -p logs
+
+  cd ../minare
+  mvn clean package -DskipTests
+  cd ../docker
+
+  docker compose build --no-cache haproxy app-coordinator infra worker mongodb redis kafka kafka-ui
+}
+start_minare() {
+  if $BUILD; then
+    build_minare
+  fi
   echo "Starting Minare..."
   WORKER_COUNT=${WORKER_COUNT} docker compose up -d --scale worker=${WORKER_COUNT} haproxy app-coordinator infra worker mongodb redis kafka kafka-ui
-elif $START_EVENNIA; then
+}
+
+build_evennia() {
+  echo "Building Evennia..."
+  docker compose build --no-cache evennia
+}
+start_evennia() {
+  if $BUILD; then
+    build_evennia
+  fi
   echo "Starting Evennia..."
   docker compose up -d evennia
+}
+
+if $START_MINARE && $START_EVENNIA; then
+  start_minare
+  start_evennia
+elif $START_MINARE; then
+  start_minare
+elif $START_EVENNIA; then
+  start_evennia
 else
-  echo "Usage: ./run.sh [WORKER_COUNT] -m|-e"
+  echo "Usage: ./run.sh [WORKER_COUNT] -b -m|-e"
   echo "  -m: Start Minare services"
   echo "  -e: Start Evennia service"
+  echo "  -b: Build"
   exit 1
 fi
 
